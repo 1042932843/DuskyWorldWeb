f1e38196adb032cf2e47a73563a12655d973abd4
dusky_dyhl <1042932843@qq.com>
2018-11-20 11:06:55 +0800

    调整结构

--- dsy/src/main/java/dsy/controller/UserController.java
+++ dsy/src/main/java/dsy/controller/UserController.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/homepage")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/index")
     public String index(){
-        return "user/index";
+        return "homepage/index";
     }
 
 }
